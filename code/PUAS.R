6c36244465e5a9577d9619480b9678600c5c89df
tengzhangs <147488840+tengzhangs@users.noreply.github.com>
2025-06-11 10:55:12 +0800

    Update PUAS.R

--- code/PUAS.R
+++ code/PUAS.R
@@ -1,75 +1,62 @@
 library(xgboost)
 library(stringr)
-#f1 <- "F:\\m6Adisease_association_reasearch\\PU_learning_AdaSampling\\m6A_embeding.csv"
-#f1 <-  "/home/disk3/zhangteng/m6A_disease/m6Asites_sim_infor/m6Asites_sim_PCA_infor.csv"
-f1 <- "F:\\m6Adisease_data\\new_dis_m6A_data\\m6Asites_rwr.csv"
-# m6Afeat <- read.csv(f1)
-# colnames(m6Afeat) <- NULL
-# #f2 <- "F:\\m6Adisease_association_reasearch\\PU_learning_AdaSampling\\dis_embeding.csv"
-# f2 <- "F:\\m6Adisease_data\\new_dis_m6A_data\\m6Adis_rwr.csv"
-# disfeat <- read.csv(f2)
-# 
-# 
-# 
-f3 <- "F:\\m6Adisease_data\\new_dis_m6A_data\\m6Adis_asso.csv"
-m6Adis_asso <- read.csv(f3)
-m6Adis_asso <- m6Adis_asso[,-c(1:5)]
-label=as.vector(t(as.matrix(m6Adis_asso)))
-# 
-# nodes_feature <- function(num_node,node_feat){
-#   colnames(node_feat) <- c("source" ,   "target" ,   "feature_value")
-#   nodes_feat <- data.frame()
-#   for (i in 1:num_node) {
-#     one_site <- node_feat[node_feat$source==i,]
-#     one_site <- one_site[order(one_site$target,decreasing = F),]
-#     source_label <- seq(1,length(unique(node_feat$source)))
-#     one_site_feat <- vector(length = length(source_label))
-#     one_site_feat[as.numeric(as.character(one_site$target))] <- as.numeric(as.character(one_site$feature_value))
-#     nodes_feat <- rbind(nodes_feat,one_site_feat)
-#   }
-#   colnames(nodes_feat) <- source_label
-#   return(nodes_feat)
-# }
-# m6Afeats <- nodes_feature(num_node=nrow(m6Adis_asso),node_feat=m6Afeat)
-# disfeats <- nodes_feature(num_node=ncol(m6Adis_asso),node_feat=disfeat)
-##normalize
+#####Transform the formation of m6A-disease associations
+m6Adis_asso_infor <- function(known_asso,m6A_asso,dis_asso){
+  m6Adis_asso_infor <- data.frame()
+  for (i in 1:nrow(m6Adis_asso)) {
+    
+    pos_label <- which(m6Adis_asso[i,]==1)
+    m6A_dis_noasso <- data.frame()
+    for (j in 1:length(pos_label)) {
+      one_dis_asso <- dis_asso[dis_asso$source==pos_label[j],]
+      one_dis_asso <- one_dis_asso[order(one_dis_asso$target,decreasing = F),]
+      if(length(which(!is.na(match(one_dis_asso$target,pos_label))))>0){
+        one_dis_asso <- one_dis_asso[-which(!is.na(match(one_dis_asso$target,pos_label))),]
+      }
+      
+      
+      target_dis <- as.numeric(as.character(one_dis_asso$target))
+      m6A_sites <- rep(i,length(target_dis))
+      one_m6A_dis_noasso <- data.frame(m6A=m6A_sites,dis=target_dis,label=rep(0,length(target_dis)))
+      
+      m6A_dis_noasso <- rbind(m6A_dis_noasso,one_m6A_dis_noasso)
+    }
+    m6A_dis_noasso <- m6A_dis_noasso[order(m6A_dis_noasso$dis,decreasing = F),]
+    m6A_dis_neg <- unique(m6A_dis_noasso$dis)
+    neg_label <- rep(0,length(m6A_dis_neg))
+    m6A_dis_infor <- c(pos_label,m6A_dis_neg)
+    m6A_dis_infor <- m6A_dis_infor[order(m6A_dis_infor,decreasing = F)]
+    pos_site <- which(!is.na(match(m6A_dis_infor,pos_label)))
+    all_lable <- vector(length = length(m6A_dis_infor))
+    all_lable[pos_site] <- 1
+    one_asso_infor <- data.frame(m6A=rep(i,length(m6A_dis_infor)),dis=m6A_dis_infor,label=all_lable)
+    m6Adis_asso_infor <- rbind(m6Adis_asso_infor,one_asso_infor)
+  }
+  return(m6Adis_asso_infor)
+}
+####Combine the embedding features for m6A sites and diseases
 
-###
-m6Afeats <- read.csv(file = "F:\\m6Adisease_data\\new_dis_m6A_data\\only_use_simvalue\\m6A_feature.csv",header = F)
-colnames(m6Afeats) <- NULL
-disfeats <- read.csv(file = "F:\\m6Adisease_data\\new_dis_m6A_data\\only_use_simvalue\\dis_feature.csv",header = F)
-colnames(disfeats) <- NULL
+comb_feats <- function(m6Afeats,disfeats,m6Adis_asso_infor){
+  colnames(m6Afeats) <- NULL
+  colnames(disfeats) <- NULL
+  new_m6A_dis_feat <- data.frame()
+  for(i in 1:nrow(m6Afeats)){
+    one_select_dis <- as.numeric(as.character(m6Adis_asso_infor[m6Adis_asso_infor$m6A==i,]$dis))
+    one_m6A_dis_feat <- disfeats[one_select_dis,]
+    one_m6A_feat <- m6Afeats[rep(i,nrow(one_m6A_dis_feat)),]
+    rownames(one_m6A_feat) <- NULL
+    onem6A_dis_feats <- cbind(one_m6A_feat,one_m6A_dis_feat)
+    new_m6A_dis_feat <- rbind(new_m6A_dis_feat,onem6A_dis_feats)
+  }
+  new_label <- as.numeric(as.character(m6Adis_asso_infor$label))
+  data=cbind(new_m6A_dis_feat,new_label)
+  colnames(data) <- c(1:ncol(new_m6A_dis_feat),"Target_label")
+  rownames(data) <- NULL
+  return(data)
+}
 
-RR=m6Afeats[rep(1:nrow(m6Afeats),each=nrow(disfeats)),]
-dd=disfeats[rep(1:nrow(disfeats),nrow(m6Afeats)),]
-feature=cbind(RR,dd)
-data=cbind(feature,label)
-colnames(data) <- c(1:ncol(feature),"Target_label")
-rownames(data) <- NULL
-library(AdaSampling)
-data.mat <- apply(X = data[,-ncol(data)], MARGIN = 2, FUN = as.numeric)
-data.cls <- label
-rownames(data.mat) <- paste("p", 1:nrow(data.mat), sep="_")
-set.seed(1)
-pos <- which(data.cls == 1)
-neg <- which(data.cls == 0)
-# Identify positive and negative examples 
-Ps <- rownames(data.mat)[which(data.cls == 1)]
-Ns <- rownames(data.mat)[which(data.cls == 0)]
-train.mat=data.mat
-test.mat=data.mat[which(!is.na(match(rownames(data.mat),Ns))),]
-
-
-# initialize sampling probablity
-pos.probs <- rep(1, length(Ps))
-una.probs <- rep(1, length(Ns))
-names(pos.probs) <- Ps
-names(una.probs) <- Ns
-
-sampleFactor = 1
-
-i <- 0
-xgboost_train <- function(Ps, Ns, dat, test=NULL, pos.probs=NULL, una.probs=NULL,  sampleFactor, seed){
+###Using XGboost model to train
+xgboost_train <- function(Ps, Ns, dat, test=NULL, pos.probs=NULL, una.probs=NULL,  sampleFactor, seed,alpha){
   set.seed(seed)
   positive.train <- c()
   positive.cls <- c()
@@ -78,13 +65,13 @@ xgboost_train <- function(Ps, Ns, dat, test=NULL, pos.probs=NULL, una.probs=NULL
   sampleN <- ifelse(length(Ps) < length(Ns), length(Ps), length(Ns))
   
   # bootstrap sampling to build the positive training set (labeled as 'P')
-  idx.pl <- unique(sample(x=Ps, size=sampleFactor*sampleN, replace=TRUE, prob=pos.probs[Ps]))
+  idx.pl <- unique(sample(x=Ps, size=sampleFactor*sampleN, replace=T, prob=pos.probs[Ps]))
   # positive.train <- dat[Ps,]
   positive.train <- dat[idx.pl,]
   positive.cls <- rep("P", nrow(positive.train))
   
   # bootstrap sampling to build the "unannotate" or "negative" training set (labeled as 'N')
-  idx.dl <- unique(sample(x=Ns, size=sampleFactor*sampleN, replace=TRUE, prob=una.probs[Ns]))
+  idx.dl <- unique(sample(x=Ns, size=sampleFactor*sampleN, replace=T, prob=una.probs[Ns]))
   unannotate.train <- dat[idx.dl,]
   unannotate.cls <- rep("N", nrow(unannotate.train))
   
@@ -94,12 +81,17 @@ xgboost_train <- function(Ps, Ns, dat, test=NULL, pos.probs=NULL, una.probs=NULL
   cls <- as.factor(c(positive.cls, unannotate.cls))
   
   ##xgboost
-  xgb.fit = xgboost(data = train.sample, label =  ifelse(cls=="P",1,0), nrounds = 3 ,
-                    objective = "binary:logistic",max_depth = 10, eta = 1,eval_metric ="auc",verbose = 1)
-  classification = predict(xgb.fit, dat)
+  xgb.fit = xgb.cv(data = train.sample, label =  ifelse(cls=="P",1,0), nrounds = 10 ,nfold=5,
+                   objective = "binary:logistic", eta = 1,eval_metric ="auc",max_depth = 6,verbose = 0,early_stopping_rounds=3)
+  elog <- as.data.frame(xgb.fit$evaluation_log)
+  nround <- which.max(elog$test_auc_mean)
+  
+  xgb_fit = xgboost(data = train.sample, label =  ifelse(cls=="P",1,0), nrounds = nround ,
+                    objective = "binary:logistic",max_depth = 6, eta = 1,eval_metric ="auc",verbose = 0)
+  classification = predict(xgb_fit, dat)
   pro=classification
   pos_pro = pro[as.numeric(as.character(str_remove(Ps,"p_")))]
-  cutoff = quantile(pos_pro, 0.1)
+  cutoff = quantile(pos_pro, alpha)
   # rescale
   map.pred.y = pro - cutoff
   map.pred.y[map.pred.y>0] = map.pred.y[map.pred.y>0]/max(map.pred.y[map.pred.y>0])
@@ -113,38 +105,89 @@ xgboost_train <- function(Ps, Ns, dat, test=NULL, pos.probs=NULL, una.probs=NULL
   return(xgb_pred)
 }
 
-while (i < 10) {
-  # update count
-  i <- i + 1
+adpt_PU <- function(data,alpha,m6Adis_asso_infor){
+  data.mat <- apply(X = data[,-ncol(data)], MARGIN = 2, FUN = as.numeric)
+  data.cls <- new_label
+  rownames(data.mat) <- paste("p", 1:nrow(data.mat), sep="_")
+  set.seed(1)
+  pos <- which(data.cls == 1)
+  neg <- which(data.cls == 0)
+  # Identify positive and negative examples 
+  Ps <- rownames(data.mat)[which(data.cls == 1)]
+  Ns <- rownames(data.mat)[which(data.cls == 0)]
+  # train.mat=data.mat
+  # test.mat=data.mat[which(!is.na(match(rownames(data.mat),Ns))),]
   
+  
+  # initialize sampling probablity
+  pos.probs <- rep(1, length(Ps))
+  una.probs <- rep(1, length(Ns))
+  names(pos.probs) <- Ps
+  names(una.probs) <- Ns
+  
+  sampleFactor = 1
+  
+  i <- 1
+  
+  
+  pred_result <- list()
   
   xboost_result <- xgboost_train(Ps=Ps, Ns=Ns, dat=data.mat, pos.probs=pos.probs,
-                                 una.probs=una.probs,  sampleFactor=sampleFactor,seed=i)
-  pos.probs <- xboost_result[Ps, "P"]
-  una.probs <- xboost_result[Ns, "N"]
-  ##KNN
-  # knn.fit <- knn(train.sample, dat, cl=cls, k=5, prob=TRUE)
-  # p <- attr(knn.fit, "prob")
-  # idx <- which(knn.fit == "N")
-  # p[idx] <- 1- p[idx]
-  # knn.pred <- cbind(p, 1 - p)
-  # colnames(knn.pred) <- c("P", "N")
-  # rownames(knn.pred) <- rownames(dat)
-  # length(which(knn.pred[,1]>0.5))/nrow(knn.pred)
-  # update probability arrays
-  # pos.probs <- knn.pred[Ps, "P"]
-  # una.probs <- knn.pred[Ns, "N"]
+                                 una.probs=una.probs,  sampleFactor=sampleFactor,seed=1,alpha=alpha)
+  pred_result[[1]] <- xboost_result
+  while (i < 1000) {
+    # update count
+    i <- i + 1
+    
+    
+    xboost_result <- xgboost_train(Ps=Ps, Ns=Ns, dat=data.mat, pos.probs=pos.probs,
+                                   una.probs=una.probs,  sampleFactor=sampleFactor,seed=i,alpha=alpha)
+    pos.probs <- xboost_result[Ps, "P"]
+    una.probs <- xboost_result[Ns, "N"]
+    pred_result[[i]] <- c(pos.probs,una.probs)
+    
+    delts <- mean(abs(pred_result[[i]]-pred_result[[i-1]]))
+    print(delts)
+    
+    if(delts<0.01){
+      break
+    }
+    
+  }
+  pos_pred <- pos.probs
+  una_pred <- una.probs
+  
+  result <- vector(length = nrow(xboost_result))
+  result[as.numeric(as.character(str_remove(Ps,"p_")))] <- 1
+  select_Ns <- names(una_pred)[which(una_pred <0.5)]
+  #select_PS <- names(pos_pred)[which(pos_pred>0.5)]
+  result[as.numeric(as.character(str_remove(select_Ns,"p_")))] <- 1
+  #result[as.numeric(as.character(str_remove(select_PS,"p_")))] <- 1
+  last_m6Adis_asso <- data.frame(m6Adis_asso_infor,pred_label =result)
+  return(last_m6Adis_asso)
 }
-pred <- xgboost_train(Ps=Ps, Ns=Ns, dat=data.mat, pos.probs=pos.probs,
-                      una.probs=una.probs,  sampleFactor=sampleFactor,seed=1)
-pos_pred <- pred[Ps,'P']
-una_pred <- pred[Ns,"N"]
-length(which(pos_pred>0.5))
-length(which(una_pred >0.5))
-result <- vector(length = nrow(pred))
-result[as.numeric(as.character(str_remove(Ps,"p_")))] <- 1
-select_Ns <- names(una_pred)[which(una_pred >0.5)]
-result[as.numeric(as.character(str_remove(select_Ns,"p_")))] <- 1
-probability=t(matrix(result,ncol=1367,nrow=457))
-Adsampling_XGboostresult <- list(pred_result=pred,new_label =probability)
-save(Adsampling_XGboostresult,file = "/home/disk3/zhangteng/m6A_disease/PU_learning/Adsampling_XGboostresult.Rdata")
+
+PUAS_result <- function(raw_m6Adis_asso,m6A_asso,dis_asso,m6Afeats,disfeats){
+  m6Adis_asso_infor <- m6Adis_asso_infor(known_asso=raw_m6Adis_asso,m6A_asso=m6A_asso,dis_asso=dis_asso)
+  data <- comb_feats(m6Afeats=m6Afeats,disfeats=disfeats,m6Adis_asso_infor=m6Adis_asso_infor)
+  PUAS_proc <- adpt_PU(data,alpha=0.1,m6Adis_asso_infor)
+  return(PUAS_proc)
+}
+
+f1 <- "./m6Adis_asso.csv"
+m6Adis_asso <- read.csv(f1)
+m6Adis_asso <- m6Adis_asso[,-c(1:5)]
+# label=as.vector(t(as.matrix(m6Adis_asso)))
+
+f2 <- "./m6Asites_RWR.csv"
+m6A_asso <- read.csv(f2)
+
+f3 <- "./m6Adis_rwr.csv"
+dis_asso <- read.csv(f3)
+###
+m6Afeats <- read.csv(file = "./m6A_GCN_embeding.csv")
+colnames(m6Afeats) <- NULL
+disfeats <- read.csv(file = "./disease_GCN_embeding.csv")
+colnames(disfeats) <- NULL
+PUAS_proc <- PUAS_result(raw_m6Adis_asso=m6Adis_asso,m6A_asso=m6A_asso,dis_asso=dis_asso,m6Afeats=m6Afeats,disfeats=disfeats)
+write.csv(PUAS_proc,file = "./PUAS_process_m6Adis_asso.csv",row.names = F)
